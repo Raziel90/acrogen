f504e84c7900cca542652c59346d58cdac9b8fff
Gokhan Solak <gknsolak@gmail.com>
2019-06-13 15:59:30 +0100

    this one add 2 letter bases like Le from Learning or At from Autonomous

--- acrogen.py
+++ acrogen.py
@@ -4,34 +4,43 @@ Spyder Editor
 
 Find Cool Acronyms for the Team Name
 """
-from itertools import combinations, permutations 
+from itertools import combinations, permutations, product
 
-Keywords = ['Cognitive', 
-            'Intelligent', 
-            'Robot', 
-            'Learning', 
-            'Autonomous', 
+Keywords = ['Cognitive',
+            'Intelligent',
+            'Robot',
+            'Learning',
+            'Autonomous',
             'Manipulation']
-            
-            
-Initials = [key[0] for key in Keywords]
+
+def isVowel(key):
+    return key.lower() in ['a','e','i','o','u']
+
+# only initials
+bases = {key:[key[0]] for key in Keywords}
+# plus pairs if not both vowel/consonnant
+for key in Keywords:
+    for letter in key:
+        if isVowel(key[0])!=isVowel(letter):
+            bases[key].append(key[0]+letter)
+    bases[key] = list(set(bases[key]))
+
+print("bases are " + str(bases))
+
 acronyms = []
 
-for acrolen in range(2,len(Keywords)):
-    
-    comb = combinations(Initials, acrolen)
-    
-    for c in comb:
-        perm = permutations(c)
-        
-        for p in perm:
-            acronyms += [''.join(p)]
-            
-            
-        
-        
+comb = list(set(product(*bases.values())))
+
+for c in comb:
+    perm = permutations(c)
+    for p in perm:
+        for acrolen in range(2,6):
+            acronyms += [''.join(p[:acrolen])]
+
+# unique vals sorted
+acronyms = list(sorted((set(acronyms))))
+
 print(str(len(acronyms)) + ' Acronyms Found!')
 
 with open('acros', 'w') as acrofile:
     acrofile.write('\n'.join(acronyms))
-    
